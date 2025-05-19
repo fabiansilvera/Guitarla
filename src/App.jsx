897e2ef921d8e12a18fdf63ff7e954a695fc6470
fabiansilvera <fabiansilvera@gmail.com>
2025-05-19 06:51:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Header from "./components/Header"
+import Guitar from "./components/Guitar"
+import { useState } from "react"
+
+function App() {
+
+  // State
+  const [auth, setAuth] = useState(false)
+  const [total, setTotal] = useState(0)
+  const [cart, setCart] = useState([])
+
+  return (
+    <>
+      <Header/>
+
+      <main className="container-xl mt-5">
+          <h2 className="text-center">Nuestra Colección</h2>
+
+          <div className="row mt-5">
+            <Guitar/>
+          </div>
+      </main>
+
+
+      <footer className="bg-dark mt-5 py-5">
+          <div className="container-xl">
+              <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
