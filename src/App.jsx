fd15dcd59206c7dd36389c84ab4478aa692b0114
fabiansilvera <fabiansilvera@gmail.com>
2025-06-06 16:15:50 -0300

    Final con el curso

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,82 @@ import Guitar from "./components/Guitar"
 import { db } from "./data/db"
 
 function App() {
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  } 
+
   //State local puede guardar db directo 
-  const [data, setData] = useState([])
+  const [data] = useState(db)
 
   // Correcto si no genermos manejar detalles especificos del carrito sino generar nuevo registro
-  //const [card, setCard] = useState([])
-  const [cart, setCard] = useState([])
+  const [cart, setCard] = useState(initialCart)
+
+  const MAX_ITEMS = 10
+
+    // useEffect para API no es el caso pero hay que practicar
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   function addToCart(item) {
-    const itemExists = cart.findIndex((guitar) => guitar.id === item.id)
-    if(itemExists !== -1) {
-      item.quantity += 1  
-      console.log("Ya existe en el carrito")
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+    if(itemExists >= 0) {
+      if(cart[itemExists].quantity >= MAX_ITEMS) return
+      const updateCart = [...cart]
+      updateCart[itemExists].quantity++
+      setCard(updateCart)
     } else {
       item.quantity = 1  
-      setCard((prevCart) => [...prevCart, item])
+      setCard([...cart, item])
     }
   }
 
-  // useEffect para API no es el caso pero hay que practicar
-  useEffect(() => {
-    setData(db)
-  }, [])
+  function removeFromCart(id) {
+    setCard(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+
+    setCard(updatedCart)
+  }
+
+  function decrementQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity > 1) {
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+
+    setCard(updatedCart)
+  }
+
+  function clearCart() {
+    setCard([])
+  }
 
   return (
     <>
-      <Header/>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decrementQuantity={decrementQuantity}
+        clearCart={clearCart}
+      />
 
       <main className="container-xl mt-5">
           <h2 className="text-center">Nuestra Colección</h2>
